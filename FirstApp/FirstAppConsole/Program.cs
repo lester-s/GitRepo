9572a6fa32406b3ad56618bd5270a045016ad1e7
lester-s <lester.simon.b@gmail.com>
2016-04-01 13:52:20 +0200

    Valeur min console print

--- FirstApp/FirstAppConsole/Program.cs
+++ FirstApp/FirstAppConsole/Program.cs
@@ -8,9 +8,13 @@ namespace FirstAppConsole
 {
     class Program
     {
+        private int minValue;
+        private int maxValue;
+        private int currentValue;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            Console.WriteLine(@"Saisir la valeur minimale:");
         }
     }
 }
